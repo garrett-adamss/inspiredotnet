cfa7b571ec789f8167b21ada9a442fa88bad442b
garrett_adamss <garrettadams27@gmail.com>
2022-11-28 23:37:20 -0700

    clean slate

--- inspiredotnet.client/src/App.vue
+++ inspiredotnet.client/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <header>
-    <Navbar />
   </header>
   <main>
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
+    <footer>
+      <div class="text-center p-2 footer-text">
+        Made by garrett-adamss 
+        <a class="footer-source" href="https://github.com/garrett-adamss/buglogger">
+          &lt;source code/&gt;
+        </a>
+      </div>
+    </footer>
 </template>
 
 <script>
@@ -30,7 +34,29 @@ export default {
 :root{
   --main-height: calc(100vh - 32px - 64px);
 }
+a {
+  color: black;
+  text-decoration: none;
+}
 
+a:hover {
+  color: black;
+}
+.footer-text{
+  position: relative;
+  bottom: 8px;
+  font-size: 16px;
+}
+
+footer a {
+    transition: transform 250ms;
+}
+
+footer a:hover {
+  background-color: #555;
+  color: white;
+  transform: translateY(-10px);
+}
 
 footer {
   display: grid;
